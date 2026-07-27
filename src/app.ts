6264ac51b5305ff7d16f61a6011e64c21ede7cc1
MELISSA DA COSTA RODRIGUES <a95704614@rn.senac.local>
2026-07-27 17:38:40 -0300

    Atualizar app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import animalRoutes from "./routes/animalRoutes";
 import consultaRoutes from "./routes/consultaRoutes";
+import donoRoutes from "./routes/donoRoutes";
+import veterinarioRoutes from "./routes/veterinarioRoutes";
 
 const app = express();
 
@@ -12,10 +14,12 @@ app.get("/", (req, res) => {
     sucesso: true,
     mensagem: "API Clínica Veterinária funcionando!"
   });
-});
+}); 
 
 app.use("/api/animais", animalRoutes);
 app.use("/api/consultas", consultaRoutes);
-
+app.use("/api/donos", donoRoutes);
+app.use("/api/veterinarios", veterinarioRoutes);
 
 export default app;
+
