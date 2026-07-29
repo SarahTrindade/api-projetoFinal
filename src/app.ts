6872c3d2ba770ed45481d920763940c0f2b747f3
sarah <sarahpias04@gmail.com>
2026-07-29 11:11:05 -0300

    atualizar css e app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,19 @@
 import express from "express";
-
+import path from "path";
 import animalRoutes from "./routes/animalRoutes";
 import consultaRoutes from "./routes/consultaRoutes";
+import donoRoutes from "./routes/donoRoutes";
+import veterinarioRoutes from "./routes/veterinarioRoutes"
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
-  res.json({
-    sucesso: true,
-    mensagem: "API Clínica Veterinária funcionando!"
-  });
-});
 
 app.use("/api/animais", animalRoutes);
 app.use("/api/consultas", consultaRoutes);
-
+app.use("/api/donos", donoRoutes);
+app.use("/api/veterinarios", veterinarioRoutes);
 
 export default app;
