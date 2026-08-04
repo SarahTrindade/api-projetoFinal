9afbdd481ddaa15f06b03bb9b11dd144203fbaa9
SARAH TRINDADE DOS SANTOS <a95717389@rn.senac.local>
2026-08-04 16:23:32 -0300

    Atualizar Middlewares

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -2,16 +2,14 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
+    destination: (req, file, cb) => {
+        cb(null, "public/uploads");
+    },
 
-  filename: (req, file, cb) => {
-    const nome = Date.now() + path.extname(file.originalname);
-    cb(null, nome);
-  },
+    filename: (req, file, cb) => {
+        const nome = Date.now() + path.extname(file.originalname);
+        cb(null, nome);
+    }
 });
 
-const upload = multer({ storage });
-
-export default upload;
+export default multer({ storage });
